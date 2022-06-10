9ff69c2a16596540d491eb440ba604be0be3c588
utsavk28 <utsavkhatu416@gmail.com>
2022-06-11 01:12:19 +0530

    Minor Fixes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-const styles = {
-    'profile-link':
-        'block p-6 max-w-sm bg-pink rounded-lg border border-gray-200 shadow-md   transition ease-in-out delay-100 bg-gradient-to-r from-sky-500 to-indigo-500  hover:-translate-y-1 hover:scale-110  hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500  duration-300',
-};
+import { profile_styles as styles, profile_data as data } from './data';
 
 const Profile = () => {
-    const data = {
-        profile_options: [
-            {
-                title: 'Schedule Meet',
-                description:
-                    'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-                link: '/schedule-meet',
-            },
-            {
-                title: 'Upload Documents',
-                description:
-                    'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-                link: '#',
-            },
-            {
-                title: 'Apply to AID',
-                description:
-                    'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-                link: '/apply-aid',
-            },
-        ],
-    };
-
     return (
         <div className='h-screen overflow-x-scroll  bg-gray-200 p-10 '>
             <div className='grid items-center justify-center lg:grid-cols-3 md:grid-cols-2 gap-4 place-items-center m-auto'>
--- client/src/components/profile/ScheduleMeet.js
+++ client/src/components/profile/ScheduleMeet.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
-const styles = {
-    link: 'inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ',
-};
+import { schedule_meet_styles as styles } from './data';
 
 const ScheduleMeet = () => {
     return (
